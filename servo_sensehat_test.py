372f8b542e5e45bf6a52c519574fac1fb5f81fe1
hernandezurbina <hernandezurbina@gmail.com>
2020-03-20 10:59:27 +0000

    Experimenting w sensehat and servo motor

--- servo_sensehat_test.py
+++ servo_sensehat_test.py
@@ -1,5 +1,9 @@
 import RPi.GPIO as GPIO
 from time import sleep
+from sense_hat import SenseHat
+
+sense = SenseHat()
+sense.set_rotation(180)
 
 GPIO.setmode(GPIO.BOARD)
 # this must change accordingly and taking care that we dont mess w
@@ -14,12 +18,14 @@ servo1.start(0)
 try:
     while True:
         # ask user for angle and turn servo to it
-        angle = float(input('Enter angle between 0 and 180: '))
+        angle = float(input('Enter angle between 0 and 180: '))        
         servo1.ChangeDutyCycle(2 + (angle / 18))
+        sense.show_message(str(angle))
         sleep(0.5)
         servo1.ChangeDutyCycle(0)
 except KeyboardInterrupt:
         # clean up things before leaving
         servo1.stop()
         GPIO.cleanup()
-        print('Bye!')        
+        sense.clear()
+        print('Bye!')
