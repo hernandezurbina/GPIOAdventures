dc1aeaa9bdb71b6ceed808a8d9c0eeec0bed0a99
hernandezurbina <hernandezurbina@gmail.com>
2023-02-15 10:41:39 +0100

    Testing standing and measuring accelerometer

--- standingUpTest.py
+++ standingUpTest.py
@@ -1,15 +1,34 @@
 import numpy as np
 import pi_servo_hat
+import qwiic_kx13x
 import sys
 from time import sleep
 
-def runTest(test):    
+def runTest(test):
+
+    secsBetweenSteps = 5
+    secsBetweenPhases = 10
+    secsBetweenMotionAndReading = 3
+
+    myKx = qwiic_kx13x.QwiicKX132()
+
+    if myKx.connected == False:
+        print("The Qwiic KX13X Accelerometer device isn't connected to the system. Please check your connection", file=sys.stderr)
+        return
+
+    
+    print('Getting first acc reading...')
+    myKx.get_accel_data()
+    print("({:.3f}g, {:.3f}g, {:.3f}g)".format(myKx.kx132_accel.x, myKx.kx132_accel.y, myKx.kx132_accel.z))  
+    print('Starting...')
+    sleep(5)
 
     while True:
         # standing phase
         print('Standing...')
         order = list(np.random.permutation(4))
         while len(order) > 0:
+
             servoNumber = (order.pop(0)) + 1 # servos are 1, 2, 3, and 4
             print(servoNumber)
             if servoNumber == 1:
@@ -20,14 +39,23 @@ def runTest(test):
                 test.move_servo_position(servoNumber, 90) # check that this value is correct
             elif servoNumber == 4:
                 test.move_servo_position(servoNumber, 0) # check that this value is correct
-            sleep(1)
 
-        sleep(5)
+            # take reading after move
+            sleep(secsBetweenMotionAndReading)
+            myKx.get_accel_data()
+            print("({:.3f}g, {:.3f}g, {:.3f}g)".format(myKx.kx132_accel.x, myKx.kx132_accel.y, myKx.kx132_accel.z))              
+
+            # sleep before next move
+            sleep(secsBetweenSteps)
+
+        print('Pausing for {} secs...'.format(secsBetweenPhases))
+        sleep(secsBetweenPhases)
 
         # sitting phase
         print('Sitting...')
         order = list(np.random.permutation(4))
         while len(order) > 0:
+
             servoNumber = (order.pop(0)) + 1 # servos are 1, 2, 3, and 4
             print(servoNumber)
             ### now values should be different to what we used for standing phase
@@ -39,16 +67,22 @@ def runTest(test):
                 test.move_servo_position(servoNumber, 0) # check that this value is correct
             elif servoNumber == 4:
                 test.move_servo_position(servoNumber, 90) # check that this value is correct
-            sleep(1)
 
-        sleep(5)
+            # take reading after move
+            sleep(secsBetweenMotionAndReading)
+            myKx.get_accel_data()
+            print("({:.3f}g, {:.3f}g, {:.3f}g)".format(myKx.kx132_accel.x, myKx.kx132_accel.y, myKx.kx132_accel.z)) 
+
+            # sleep before next move
+            sleep(secsBetweenSteps)
+
+        print('Pausing for {} secs...'.format(secsBetweenPhases))
+        sleep(secsBetweenPhases)
 
 if __name__ == '__main__':
     try:
         test = pi_servo_hat.PiServoHat()
-        print('Starting...')
         test.restart()
-        sleep(5)
         runTest(test)
     except (KeyboardInterrupt, SystemExit) as exErr:
         print('\nFinishing test.')
