b78add4c1262bef6ad6d18122faf5c05e64bd7bb
Victor Hernandez-Urbina <hernandezurbina@gmail.com>
2025-11-24 09:10:14 +0000

    Added new code to compute pitch and roll angle

--- accelerometerTest.py
+++ accelerometerTest.py
@@ -1,19 +1,12 @@
+#!/usr/bin/env python3
 import qwiic_kx13x
 import time
 import sys
-import RPi.GPIO
-
-
-"""
-This works more or less. To run it you need to put the phat in top of the rpi,
-connect the accelerometer to the qwiic port, and finally open the virtualenv venv
-"""
-
+import math
 
 def runExample():
 
     print("\nSparkFun KX13X Accelerometer Example 1\n")
-    # myKx = qwiic_kx13x.QwiicKX134() # If using the KX134 un-comment this line and replace other instances of "kx132" with "kx134"
     myKx = qwiic_kx13x.QwiicKX132()
 
     if myKx.connected == False:
@@ -26,22 +19,39 @@ def runExample():
     else:
         print("Make sure you're using the KX132 and not the KX134")
 
-    # myKx.set_range(myKx.KX132_RANGE8G) # Update the range of the data output.
-    #myKx.initialize(myKx.BASIC_SETTINGS) # Load basic settings
-    myKx.initialize(myKx.DEFAULT_SETTINGS) # Load basic settings 
+    if (myKx.software_reset()):
+        print("Reset")
+
+    # Many settings for KX13X can only be
+    # applied when the accelerometer is powered down.
+    # However there are many that can be changed "on-the-fly"
+    # check datasheet for more info
+    myKx.enable_accel(False)
+    # myKx.set_range(myKx.KX132_RANGE16G)
+    myKx.set_range(myKx.KX132_RANGE2G)
+    myKx.enable_data_engine()
+    myKx.enable_accel()
 
     while True:
+        if myKx.data_ready():    
+            myKx.get_accel_data()
+            ax = myKx.kx132_accel.x
+            ay = -1 * myKx.kx132_accel.y
+            az = myKx.kx132_accel.z
+            #print("X: {0} Y: {1} Z: {2}".format(ax, ay, az))
+            
+            roll  = math.atan2(ay, az)
+            pitch = math.atan2(-ax, math.sqrt(ay*ay + az*az))
 
-        myKx.get_accel_data()
-        print("X: {:.2f}g Y: {:.2f}g Z: {:.2f}g".format(myKx.kx132_accel.x,
-                                               myKx.kx132_accel.y,
-                                               myKx.kx132_accel.z))
-        time.sleep(.02) #Set delay to 1/Output Data Rate which is by default 50Hz 1/50 = .02
-
+            roll_deg  = math.degrees(roll)
+            pitch_deg = math.degrees(pitch)            
+            
+            print("Roll: {:.2f}°, Pitch: {:.2f}°".format(roll_deg, pitch_deg))
+            time.sleep(.02) #Set delay to 1/Output Data Rate which is by default 50Hz 1/50 = .02
 
 if __name__ == '__main__':
-        try:
-            runExample()
-        except (KeyboardInterrupt, SystemExit) as exErr:
-            print("\nEnding Example 1")
-            sys.exit(0)
+	try:
+		runExample()
+	except (KeyboardInterrupt, SystemExit) as exErr:
+		print("\nEnding Example 1")
+		sys.exit(0)
--- standingUpTest.py
+++ standingUpTest.py
@@ -2,24 +2,58 @@ import numpy as np
 import pi_servo_hat
 import qwiic_kx13x
 import sys
+import math
 from time import sleep
+
+def get_roll_pitch(myKx):
+    roll_deg = -1
+    pitch_deg = -1
+    
+    if myKx.data_ready():    
+        myKx.get_accel_data()
+        ax = myKx.kx132_accel.x
+        ay = -1 * myKx.kx132_accel.y
+        az = myKx.kx132_accel.z
+            
+        roll  = math.atan2(ay, az)
+        pitch = math.atan2(-ax, math.sqrt(ay*ay + az*az))
+
+        roll_deg  = math.degrees(roll)
+        pitch_deg = math.degrees(pitch)
+    return roll_deg, pitch_deg
+    
 
 def runTest(test):
 
     secsBetweenSteps = 5
     secsBetweenPhases = 10
-    secsBetweenMotionAndReading = 3
+    secsBetweenMotionAndReading = 1
 
+    # initializing the accelerometer
+    print("initializing accelerometer")
     myKx = qwiic_kx13x.QwiicKX132()
 
     if myKx.connected == False:
         print("The Qwiic KX13X Accelerometer device isn't connected to the system. Please check your connection", file=sys.stderr)
         return
 
+    if myKx.begin():
+        print("Ready.")
+    else:
+        print("Make sure you're using the KX132 and not the KX134")
+
+    if (myKx.software_reset()):
+        print("Reset")
+        
+    myKx.enable_accel(False)
+    myKx.set_range(myKx.KX132_RANGE2G)
+    myKx.enable_data_engine()
+    myKx.enable_accel()
     
     print('Getting first acc reading...')
-    myKx.get_accel_data()
-    print("({:.3f}g, {:.3f}g, {:.3f}g)".format(myKx.kx132_accel.x, myKx.kx132_accel.y, myKx.kx132_accel.z))  
+    roll_deg, pitch_deg = get_roll_pitch(myKx)
+    print("Roll: {:.2f}°, Pitch: {:.2f}°".format(roll_deg, pitch_deg))  
+    
     print('Starting...')
     sleep(5)
 
@@ -42,9 +76,9 @@ def runTest(test):
 
             # take reading after move
             sleep(secsBetweenMotionAndReading)
-            myKx.get_accel_data()
-            print("({:.3f}g, {:.3f}g, {:.3f}g)".format(myKx.kx132_accel.x, myKx.kx132_accel.y, myKx.kx132_accel.z))              
-
+            roll_deg, pitch_deg = get_roll_pitch(myKx)
+            print("Roll: {:.2f}°, Pitch: {:.2f}°".format(roll_deg, pitch_deg)) 
+            
             # sleep before next move
             sleep(secsBetweenSteps)
 
@@ -70,15 +104,15 @@ def runTest(test):
 
             # take reading after move
             sleep(secsBetweenMotionAndReading)
-            myKx.get_accel_data()
-            print("({:.3f}g, {:.3f}g, {:.3f}g)".format(myKx.kx132_accel.x, myKx.kx132_accel.y, myKx.kx132_accel.z)) 
-
+            roll_deg, pitch_deg = get_roll_pitch(myKx)
+            print("Roll: {:.2f}°, Pitch: {:.2f}°".format(roll_deg, pitch_deg)) 
             # sleep before next move
             sleep(secsBetweenSteps)
 
         print('Pausing for {} secs...'.format(secsBetweenPhases))
         sleep(secsBetweenPhases)
 
+###############################################################################################
 if __name__ == '__main__':
     try:
         test = pi_servo_hat.PiServoHat()
