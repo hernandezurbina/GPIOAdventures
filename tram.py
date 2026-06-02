d8a6b8e1514eb196c786b8b3b7fbadf7f725fb2c
hernandezurbina <hernandezurbina@gmail.com>
2026-06-02 09:53:00 +0200

    Added new func and train stops to display

--- tram.py
+++ tram.py
@@ -3,33 +3,31 @@ import json
 from datetime import datetime
 from sense_hat import SenseHat
 
+sense = SenseHat()
+sense.set_rotation(180)
 
-def main():
-    sense = SenseHat()
-    sense.set_rotation(180)
+def call_and_display(_from, _to):
+    stops = {"Buschallee": 900140519,
+             "Weisser See": 900140006,
+             "Hackescher Markt": 900100002,
+             "Am Kupfergraben": 900100038,
+             "Virchow-Klinikum": 900010251,
+             "Warschauerstr": 900120011
+            }    
     
-    stops = {"buschallee": 900140519,
-             "weisser see": 900140006,
-             "hackescher markt": 900100002,
-             "am kupfergraben": 900100038,
-             "virchow-klinikum": 900010251,
-             "warschauerstr": 900120011
-            }
-            
     url = "https://v6.bvg.transport.rest/stops/{}/departures?direction={}&duration=10" # all incoming traffic within 10 mins
-    
-    ### M4
-    response = requests.get(url.format(stops["buschallee"], stops["hackescher markt"]))
+    response = requests.get(url.format(stops[_from], stops[_to]))
     
     if response.status_code == 200:
         data = response.json()
         #print(json.dumps(response.json(), indent=2))
     else:
-        print("Request failed:", response.status_code)
+        print(f"({_from} -> {_to}) Request failed:", response.status_code)
         return
 
-    if len(data["departures"]) > 0:
-        print("M4: Buschallee->Hackescher Markt. There are", len(data["departures"]), "departures.")
+    if len(data["departures"]) > 0:        
+        line = data["departures"][0]["line"]["name"]
+        print(f"{line}: ({_from} -> {_to}) There are", len(data["departures"]), "departures.")        
         when_str = data["departures"][0]["when"]
         departure_time = datetime.fromisoformat(when_str)
         now = datetime.now(departure_time.tzinfo)
@@ -37,7 +35,22 @@ def main():
         print(f"Train arrives in {minutes_until:.1f} minutes")
         
         # display in sensehat:
-        sense.show_message(f"M4: Buschallee->Hackescher Markt {minutes_until:.1f} mins")
+        sense.show_message(f"{line}: {_from}->{_to} {minutes_until:.1f} mins")
+    return
+
+def main():
+    
+    
+    ### M4
+    call_and_display("Buschallee", "Hackescher Markt")
+
+    ### M12
+    call_and_display("Weisser See", "Am Kupfergraben")
+
+    ### M13
+    call_and_display("Weisser See", "Warschauerstr")
+    
+    
     return
 
 
